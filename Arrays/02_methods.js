7511fc25c6ae5f63dfe56c65bac9021bcbcdb6d2
akshay07-max <akshaysurase992@gmail.com>
2026-01-03 10:26:30 +0530

    Array Methods

---
+++ Arrays/02_methods.js
@@ -0,0 +1,22 @@
+// methods 1. push, 2. pop, 3. unshift, 4. shift, 5. splice, 6. slice, 7. concat, 8. join, 9. indexOf, 10. includes, 11. sort, 12. reverse
+
+// creating an array
+
+const myArr = [1, 3, 5, 6, 7, 8];
+
+// push
+myArr.push(9);  // pushes the value 9 at the end of and array (adds to the end). if we store it in the variable it will return the final length of the array.
+console.log(myArr); 
+
+// pop
+myArr.pop()  // removes the last element from the array.
+console.log(myArr);
+
+// unshift
+myArr.unshift(10);  // add the element to the beginning
+console.log(myArr)
+
+
+// shift 
+myArr.shift() // like a pop for beginning. It remove the element from the beginning.
+console.log(myArr)
