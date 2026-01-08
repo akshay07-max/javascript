98881068017e992879e35527c5a2cd6c94dc1708
akshay07-max <akshaysurase992@gmail.com>
2026-01-08 20:10:53 +0530

    every method

---
+++ Arrays/Array_Iteration/12_every.js
@@ -0,0 +1,8 @@
+// check if all the elements pass the test: (boolean)
+
+const arr = [2, 4, 6, 8, 10, 12];
+
+const evenArr = arr.every((num) => num % 2 === 0);  // return true if all the elements in the array has even numbers.
+
+console.log(evenArr)
+
