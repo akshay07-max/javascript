41430a4d7c38f65cec5a5066387a7f665794f6da
akshay07-max <akshaysurase992@gmail.com>
2026-01-08 20:22:05 +0530

    all methods once

---
+++ Arrays/Array_Iteration/13_all_in.js
@@ -0,0 +1,10 @@
+const arr = [12, 22, 3, 44, 55, 34];
+
+const allIn = arr
+    .filter((num) => num % 2 === 0) // get all even 
+    .map((num) => num * 2) // make all the evens double
+    .reduce((accumulator, current) => {
+        return accumulator + current; // get the sum of the filtered and modified array
+    })
+
+    console.log(allIn) // 224 will be the output.
