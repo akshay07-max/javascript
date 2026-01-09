cd213d7f5381cba27774ced325f480aa482406eb
akshay07-max <akshaysurase992@gmail.com>
2026-01-09 23:02:27 +0530

    case conversion

---
+++ Strings/02_methods.js
@@ -0,0 +1,13 @@
+// Case Conversion
+
+const str = "My name is Anthony";
+
+console.log(`Original string is: ${str}`)
+
+// Converting string to upper case
+const upperCase = str.toUpperCase()
+console.log(upperCase);
+
+// to lowercase
+const lowerCase = str.toLowerCase()
+console.log(lowerCase) 
