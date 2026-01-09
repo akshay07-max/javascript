a6b6421affdfc927945d7fd21f3dea67e9b39e86
akshay07-max <akshaysurase992@gmail.com>
2026-01-09 23:27:29 +0530

    searching in string

---
+++ Strings/03_searching.js
@@ -0,0 +1,18 @@
+// searching
+
+const str = "The name is Khan"
+
+const getIndex = str.indexOf("Khan")
+console.log(getIndex) //12
+
+const lastIndex = str.lastIndexOf("n")
+console.log(lastIndex)
+
+const include = str.includes("name")
+console.log(include)
+
+const srtWith = str.startsWith("The")
+console.log(srtWith);
+
+const endWIth = str.endsWith("Khan")
+console.log(endWIth)
