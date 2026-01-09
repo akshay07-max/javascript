4176231ddaa3c6f2df676c950a6df309d3ef2e5e
akshay07-max <akshaysurase992@gmail.com>
2026-01-09 21:11:43 +0530

    average

--- Arrays/Arrays_Practice/11_average_of_array.js
+++ Arrays/Arrays_Practice/11_average_of_array.js
@@ -1,9 +1,11 @@
 const arr = [12, 44, 55, 66]
 console.log(arr.length)
 const sum = arr.reduce((acc, cur) => {
+    console.log(cur)
+    console.log(acc)
     return acc+cur
 }, 0) // the 0 is provided as the initial value to the reduce.
 
 
 average = sum/arr.length
-console.log(average) // 44.25
+// console.log(average) // 44.25
