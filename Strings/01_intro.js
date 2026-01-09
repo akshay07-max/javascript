0b41cb48c468d3a8274e67f9bbd092a6b3040b4a
akshay07-max <akshaysurase992@gmail.com>
2026-01-09 22:54:51 +0530

    strings intro

---
+++ Strings/01_intro.js
@@ -0,0 +1,17 @@
+// Strings are immutable sequence of the character with many built in methods.
+
+const str1 = "Hello World!";
+const str2 = 'My name is Anthony'
+const str3 = `Template Literal`
+
+
+console.log(str1);
+
+console.log(str2.length) // length of the string 2
+
+console.log(str3.charCodeAt(0))  // ASCII code of 1st char ("T")
+
+console.log(str1[0])  
+
+console.log(str1.charAt(1))
+
