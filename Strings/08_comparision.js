cf70ca66018d29705151f36483bfd6b6deed9970
akshay07-max <akshaysurase992@gmail.com>
2026-01-10 20:19:27 +0530

    strings caparision and concatination

---
+++ Strings/08_comparision.js
@@ -0,0 +1,3 @@
+// String comparison
+console.log("apple" < "banana"); // true (lexicographical)
+console.log("10" < "2"); // true (string comparison)
---
+++ Strings/09_converting_to_string.js
@@ -0,0 +1,15 @@
+const num = 10
+
+const num_to_string = String(num)
+
+console.log(num_to_string)
+
+
+
+// more ways
+console.log(num.toString());   // "42"
+console.log(`${num}`);         // "42"
+
+
+console.log(typeof num);
+console.log(typeof num_to_string);
