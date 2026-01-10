134799dac55691327c63ced5946301121e563c24
akshay07-max <akshaysurase992@gmail.com>
2026-01-10 13:42:48 +0530

    replace.js

---
+++ Strings/05_replace.js
@@ -0,0 +1,10 @@
+const text = "Hello, World"
+
+// text.replace("World", "Javascript")
+
+// console.log(text) // original text will be printed, the string is immutable. replace creates a copy of the original string.
+
+
+
+const rep = text.replace("World", "Javascript.");
+console.log(rep);
