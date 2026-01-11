afb7aef2e2bc26d948cab35942a6c639685c4543
akshay07-max <akshaysurase992@gmail.com>
2026-01-11 19:33:03 +0530

    end of questions from ES6

---
+++ ES6/Practice/05_optional_chaining.js
@@ -0,0 +1,12 @@
+const obj = {
+    name: "Akshay",
+    surname: "Surase",
+    address: {
+        city: "Mumbai",
+        pin: 400061,
+        area: "Powai, Kurla"
+    }
+}
+
+console.log(obj.address?.city)
+console.log(obj.contact?.phone)
