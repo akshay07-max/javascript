703bfa0bf21312a79ca789e55ad2bb5d96d160fb
akshay07-max <akshaysurase992@gmail.com>
2026-01-12 23:23:52 +0530

    promise all

---
+++ Async_Javascript/06_promise_all.js
@@ -0,0 +1,16 @@
+const promise1 = Promise.resolve(3);
+
+const promise2 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(5);
+    }, 1000)
+})
+
+
+const promise3 = Promise.resolve("Foo")
+
+Promise.all([promise1, promise2, promise3])
+    .then((result) => {
+    console.log(result)
+    })
+
