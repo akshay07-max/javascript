ee37dd4fdb111d5dec96b7b9b77618884541a9f2
akshay07-max <akshaysurase992@gmail.com>
2026-01-15 17:27:08 +0530

    setTimeout syntax

--- Async_Javascript/Youtube/02_timeouts.js
+++ Async_Javascript/Youtube/02_timeouts.js
@@ -17,3 +17,19 @@ console.log("I was the end...")
 // setTimeout(function, duration, param1, param2,...)
 // The first parameter is a funcntion to run, or a reference to a function defined elsewhere
 // The second parameter is a number representing the duration in milliseconds to wait before executung the code 
+
+function greet() {
+    console.log("Hello Vishwas")
+}
+
+setTimeout(greet, 2000) // setTimeout with external function and timelaps.
+
+function greet2(name) {
+    console.log(`The name is ${name}`)
+}
+
+// const getname = setTimeout(greet2, 1000, "Akshay"); // (function, duration, param(parameter for function)) 
+setTimeout(greet2, 1000, "Akshay"); // (function, duration, param(parameter for function)) 
+
+// console.log(getname);
+
