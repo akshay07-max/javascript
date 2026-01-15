a1274a2a127f789035da88018722ab249a5afe45
akshay07-max <akshaysurase992@gmail.com>
2026-01-15 17:59:09 +0530

    setInterval

---
+++ Async_Javascript/Youtube/04_setInterval.js
@@ -0,0 +1,13 @@
+// The setInterval() function repeatedly runs the same code over and again at regular Intervals
+
+//syntax:
+// setInterval(function, duration, param1, param2, ...)
+
+// the first parameter is the code to execute
+// second is duration
+// argument zero or more
+
+
+setInterval(() => {
+    console.log("Logging every 2 seconds")
+}, 2000)
