1a48fe97c6d8e4c732c8e42781e6a13a5ebe7478
akshay07-max <akshaysurase992@gmail.com>
2026-01-17 23:59:14 +0530

    dummy

---
+++ Async_Javascript/Youtube/callbacks/01_callbacks.js
@@ -0,0 +1,10 @@
+function greet(name, callback) {
+  console.log("Hello " + name);
+  callback();
+}
+
+function afterGreeting() {
+  console.log("Welcome to JavaScript!");
+}
+
+greet("Akshay", afterGreeting);
