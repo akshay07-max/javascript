91a7bf69c6793cdb537816afb8486b94f9699d13
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-18 23:17:43 +0530

    Update 05_throw.js

--- Error Handling/05_throw.js
+++ Error Handling/05_throw.js
@@ -1,1 +1,12 @@
 class ValidationError{
+    contructor(message){
+       super(message)
+       this.name="Validation Error")
+     }
+
+   throw new Error("Validation Error", this.message);
+}
+
+
+
+
