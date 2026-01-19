bfb7c66d128c0026e80f0ba75f0f703cb829191a
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-19 21:59:53 +0530

    Update 11_pract_split.js

--- Strings/11_pract_split.js
+++ Strings/11_pract_split.js
@@ -2,3 +2,10 @@ const str = "The name is Sharukh Khan";
 
 const getStr = str.split("");
 console.log(getStr)
+
+if(str.inlcudes("Shaharukh"){
+ console.log("found word");
+}
+else{
+console.log("not found");
+}
