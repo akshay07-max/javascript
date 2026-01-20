a2c72bebd758da60dca7f44e8583b50228987e60
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-20 20:30:43 +0530

    Update 19_pract.js
    array with async js

--- Async_Javascript/19_pract.js
+++ Async_Javascript/19_pract.js
@@ -5,5 +5,6 @@ const arr = ["Om", "Namaha", "Shivaay"];
 for(let i=0; i<=arr.length-1; i++){
     setTimeout(()=>{
          console.log("printing the value of elements of an array evey 1 seconds.")
+         console.log(~Element at index ${i} is ${arr[i]}~
     },1000)
 }
