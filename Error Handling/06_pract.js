7c5546e2747840406ade20950094d0612b4981e2
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-20 20:58:37 +0530

    Update 06_pract.js
    try catch

--- Error Handling/06_pract.js
+++ Error Handling/06_pract.js
@@ -1,1 +1,12 @@
 // handle the error of the function
+
+const data = (data) =>{
+    try{
+       if(data){
+        console.log("got data", data);
+     }
+
+    } catch(e){
+      console.log("no data available", e);
+    }
+}
