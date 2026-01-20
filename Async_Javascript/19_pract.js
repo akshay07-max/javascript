91e76abd8578f7a44b8ade7cb9fd7f33b4afd45a
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-20 20:28:43 +0530

    Update 19_pract.js

--- Async_Javascript/19_pract.js
+++ Async_Javascript/19_pract.js
@@ -1,2 +1,9 @@
-// use the array and promise together.
+// use the array and aysnc js together.
 
+const arr = ["Om", "Namaha", "Shivaay"];
+
+for(let i=0; i<=arr.length-1; i++){
+    setTimeout(()=>{
+         console.log("printing the value of elements of an array evey 1 seconds.")
+    },1000)
+}
